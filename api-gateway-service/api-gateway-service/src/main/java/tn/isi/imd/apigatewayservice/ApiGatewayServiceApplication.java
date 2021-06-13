de3cfd28e87e6929e58026bbb5d8d128523eb377
imed <imedzenned@yahoo.fr>
2021-06-13 02:11:13 +0200

    adding gateway

---
+++ api-gateway-service/api-gateway-service/src/main/java/tn/isi/imd/apigatewayservice/ApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package tn.isi.imd.apigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ api-gateway-service/api-gateway-service/src/test/java/tn/isi/imd/apigatewayservice/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.isi.imd.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
